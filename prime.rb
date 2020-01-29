cd4a83c0087d4a307f021ebc6da0ab5fc893d732
Leonor Colbert <leonoracolbert@gmail.com>
2020-01-29 18:21:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(number)
+  if number <= 1
+    return false
+  end
+  
+  root = Math.sqrt(number)
+  range = 2..root
+  
+  range.none? do |i|
+    number % i == 0
+  end
+  
+end
